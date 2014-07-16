0646b6bf72d073b0cbd40f3eaabb1669aec4956a
Sam Stevens <sam@xnet.tk>
2014-07-16 20:06:23 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: sam
+ *
+ * Created on 16 July 2014, 20:05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
